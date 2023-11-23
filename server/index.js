ce8df63ff1aa77a229bdae2a3e3f71cf41614171
Juan Paniagua <juan.ignacio10396@gmail.com>
2023-11-23 20:20:57 -0300

    v0.02 server first implementation

--- server/index.js
+++ server/index.js
@@ -1,3 +1,24 @@
-import { fs } from "node:fs";
+import net from "node:net";
+import dotenv from "dotenv";
+dotenv.config();
 
-console.log(fs);
+const port = process.env.PORT ?? 2323;
+
+const serverTCP = net.createServer();
+
+serverTCP.on("connection", (socket) => {
+    socket.on("data", () => {
+
+    }) 
+    socket.on("close", () => {
+        console.log("Client disconnected")
+    })
+    socket.on("error", () => {
+        console.log("Client error")
+    })
+    console.log("Client connected", new Date().toLocaleString());
+})
+
+serverTCP.listen(port, () => {
+    console.log(`Server is up on server ${port}, on date ${new Date().toLocaleString()}`)
+});
